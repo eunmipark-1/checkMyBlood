081092497eac025dd950ba2a1d42698b509d3f9e
mia <mia1221@kakao.com>
2021-03-19 04:37:33 +0900

    component  진행중

--- App.js
+++ App.js
@@ -8,32 +8,23 @@ import {
   ScrollView,
   FlatList,
 } from "react-native";
-
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalItem';
 export default function App() {
-  const [enteredGoal, setEnteredGoal] = useState("");
+  
   const [courseGoals, setCourseGoals] = useState([]);
-  const goalInputHandler = (enteredText) => {
-    setEnteredGoal(enteredText);
-  };
+  
 
-  const addGoalHandler = () => {
+  const addGoalHandler = (goalTitle) => {
     setCourseGoals((currentGoals) => [
       ...currentGoals,
-      { id: Math.random().toString(), value: enteredGoal },
+      { id: Math.random().toString(), value: goalTitle },
     ]);
   };
+  
   return (
     <View style={styles.screen}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          placeholder="Course Goal"
-          style={styles.input}
-          onChangeText={goalInputHandler}
-          value={enteredGoal}
-        />
-        <Button title="ADD" onPress={addGoalHandler} />
-      </View>
-
+      <GoalInput onAddGoal = {addGoalhandler}/>
       {/* <ScrollView>
         {courseGoals.map((goal) => (
           <View style={styles.listItem}>
@@ -45,9 +36,7 @@ export default function App() {
         keyExtractor={(item, index) => item.id}
         data={courseGoals}
         renderItem={(itemData) => (
-          <View style={styles.listItem}>
-            <Text>{itemData.item.value}</Text>
-          </View>
+         <GoalItem title={itemData.item.value}/>
         )}
       />
     </View>
@@ -58,17 +47,5 @@ const styles = StyleSheet.create({
   screen: {
     padding: 50,
   },
-  inputContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  input: { width: "80%", borderColor: "black", borderWidth: 1, padding: 10 },
-  listItem: {
-    padding: 10,
-    marginVertical: 10,
-    backgroundColor: "#ccc",
-    borderColor: "gray",
-    borderWidth: 1,
-  },
+  
 });
