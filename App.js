b465b1e8ab178696aa574429dc39dcb7eb7201d2
mia <mia1221@kakao.com>
2021-03-25 04:17:57 +0900

    add items

--- App.js
+++ App.js
@@ -1,11 +1,8 @@
 import React, { useState } from "react";
 import {
   StyleSheet,
-  Text,
   View,
-  TextInput,
   Button,
-  ScrollView,
   FlatList,
 } from "react-native";
 import GoalItem from './components/GoalItem';
@@ -13,17 +10,33 @@ import GoalInput from './components/GoalInput';
 export default function App() {
   
   const [courseGoals, setCourseGoals] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false); 
 
   const addGoalHandler = (goalTitle) => {
+    if(goalTitle.length === 0) {
+      return;
+    }
     setCourseGoals((currentGoals) => [
       ...currentGoals,
       { id: Math.random().toString(), value: goalTitle },
     ]);
+    setIsAddMode(false);
   };
   
+  const removeGoalHandler =  (goalId) => {
+    console.log('TO BE DELETED : ' + goalId);
+    setCourseGoals( currentGoals => {
+      return currentGoals.filter((goal) => goal.id !== goalId);
+    })
+  };
+
+  const cancelGoalAddtionHandler = () => {
+    setIsAddMode(false);
+  }   
   return (
     <View style={styles.screen}>
-      <GoalInput onAddGoal={addGoalHandler}/>
+      <Button title="Add New Goal" onPress={() => setIsAddMode(true)}/>
+      <GoalInput visible={isAddMode} onAddGoal={addGoalHandler} onCancel={cancelGoalAddtionHandler}/>
       {/* <ScrollView>
         {courseGoals.map((goal) => (
           <View style={styles.listItem}
@@ -35,7 +48,7 @@ export default function App() {
         keyExtractor={(item, index) => item.id}
         data={courseGoals}
         renderItem={(itemData) => (
-         <GoalItem title={itemData.item.value}/>
+         <GoalItem id={itemData.item.id} title={itemData.item.value} onDelete={removeGoalHandler}/>
         )}
       />
     </View>
