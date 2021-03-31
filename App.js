2c9404ab4fde875a0640674e48a26082c7e97f1a
mia <mia1221@kakao.com>
2021-03-31 23:39:12 +0900

    card형 UI  적용

--- App.js
+++ App.js
@@ -1,63 +1,21 @@
 import React, { useState } from "react";
-import {
-  StyleSheet,
-  View,
-  Button,
-  FlatList,
-} from "react-native";
-import GoalItem from './components/GoalItem';
-import GoalInput from './components/GoalInput';
+import {StyleSheet,View} from "react-native";
+import Header from './components/Header';
+import StartGameScreen from './screens/StartGameScreen';
+
 export default function App() {
-  
-  const [courseGoals, setCourseGoals] = useState([]);
-  const [isAddMode, setIsAddMode] = useState(false); 
-
-  const addGoalHandler = (goalTitle) => {
-    if(goalTitle.length === 0) {
-      return;
-    }
-    setCourseGoals((currentGoals) => [
-      ...currentGoals,
-      { id: Math.random().toString(), value: goalTitle },
-    ]);
-    setIsAddMode(false);
-  };
-  
-  const removeGoalHandler =  (goalId) => {
-    console.log('TO BE DELETED : ' + goalId);
-    setCourseGoals( currentGoals => {
-      return currentGoals.filter((goal) => goal.id !== goalId);
-    })
-  };
-
-  const cancelGoalAddtionHandler = () => {
-    setIsAddMode(false);
-  }   
   return (
     <View style={styles.screen}>
-      <Button title="Add New Goal" onPress={() => setIsAddMode(true)}/>
-      <GoalInput visible={isAddMode} onAddGoal={addGoalHandler} onCancel={cancelGoalAddtionHandler}/>
-      {/* <ScrollView>
-        {courseGoals.map((goal) => (
-          <View style={styles.listItem}
-            <Text key={goal}>{goal}</Text>
-          </View>
-        ))}
-      </ScrollView> */}
-      <FlatList
-        keyExtractor={(item, index) => item.id}
-        data={courseGoals}
-        renderItem={(itemData) => (
-         <GoalItem id={itemData.item.id} title={itemData.item.value} onDelete={removeGoalHandler}/>
-        )}
-      />
+      <Header title="Guess a Number" />
+      <StartGameScreen />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   screen: {
-    padding: 50,
+    flex:1,
+
   },
   
 });
